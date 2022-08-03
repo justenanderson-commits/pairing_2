dc5614ac7f5d049b4fdb747216933d2a46c990f2
Justen Anderson <justen.anderson@gmail.com>
2022-08-03 16:37:54 -0600

    Add good friend variable

--- friends.js
+++ friends.js
@@ -1,8 +1,10 @@
-var bff = 'Jimothy';
-var enemy = true;
+var bff = 'Timothy';
+var goodFriend = 'Jeter';
 
-if (enemy == true){
+if (bff === 'Jimothy'){
   console.log(`Hi, ${bff}!`);
+} else if (goodFriend === 'Peter') {
+  console.log(`Hi, ${goodFriend}`);
 } else {
   console.log('Who are you?');
 }
